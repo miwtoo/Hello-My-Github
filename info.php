3f4fea45fc512755c86871508bfec69bb1dc7918
Miwtoo <miwyugioh@gmail.com>
2015-04-21 19:25:35 +0700

    Create info.php

---
+++ info.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Info</title>
+</head>
+
+<body>
+	<?
+    	print"PHP_VERSION = ".PHP_VERSION."<br>";
+		print"PHP_OS = ".PHP_OS."<br>";
+		print"__FILE__ = ".__FILE__."<br><hr>";
+		
+		print"SERVER_NAME = ".$SERVER_NAME."<br>";
+		print"SERVER_PORT = ".$SERVER_PORT."<br>";
+		print"USER_SERVER = ".$USER_SERVER."<br>";
+		print"HTTP_USER_AGENT = ".$HTTP_USER_AGENT."<br>";
+		
+	?>
+</body>
+</html>
